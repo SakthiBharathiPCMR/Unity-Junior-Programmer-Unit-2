764ad45d9825cb873e3f9391d8634b6264a19f52
SakthiBharathiPCMR <sakthibharathipcmr@gmail.com>
2024-10-29 22:43:42 +0530

    2.4

--- Assets/Scripts/DestroyOutOfBounds.cs
+++ Assets/Scripts/DestroyOutOfBounds.cs
@@ -17,12 +17,14 @@ public class DestroyOutOfBounds : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //remove gameobject when it off from screen
         if (transform.position.z > upperBound)
         {
             Destroy(gameObject);
         }
         else if (transform.position.z < lowerBound)
         {
+            Debug.Log("Game Over!");
             Destroy(gameObject);
         }
 
